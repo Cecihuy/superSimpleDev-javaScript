63943aad2adf273edbe38957036ab2adc4feb3a4
Spck Editor <spck@mail.it>
2024-12-25 14:34:38 +0700

    make the cart quantity interactive

--- project/scripts/amazon.js
+++ project/scripts/amazon.js
@@ -76,5 +76,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((btn)=>{
         }
       );
     }
+    let cartQuantity = 0;
+    
+    cart.forEach((item) => {
+      cartQuantity = cartQuantity + item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
