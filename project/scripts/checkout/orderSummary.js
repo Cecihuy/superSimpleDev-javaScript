2a8295a48b6d768716fc642566f325cc1907f672
Spck Editor <spck@mail.it>
2024-12-31 05:49:01 +0700

    make payment summary interactive

--- project/scripts/checkout/orderSummary.js
+++ project/scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from '../utils/dayjsESM/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -96,6 +97,7 @@ export function renderOrderSummary() {
       removeFromCart(productId);
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+      renderPaymentSummary();
     });
   });
   document.querySelectorAll('.js-delivery-option').forEach((element) => {
@@ -103,6 +105,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOptions(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 };
